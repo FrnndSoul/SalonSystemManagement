b7a2d071e7cb3704f30e33e488a5a0a6f9d7433f
Tubachups <125182971+Tubachups@users.noreply.github.com>
2024-03-01 14:48:03 +0800

    SignInPage design done SignUpForm design done AdminForm design done GeneralView Inventory design done RequestShipment Inventory design done AddEditProduct Inventory design done

--- TriforceSalon/Properties/Resources.Designer.cs
+++ TriforceSalon/Properties/Resources.Designer.cs
@@ -259,5 +259,15 @@ namespace TriforceSalon.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap upload_icon {
+            get {
+                object obj = ResourceManager.GetObject("upload icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
