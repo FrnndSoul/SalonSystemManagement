77b294b47a96da7c7838dbdb15b63995ce0c3c92
Nathaniel Canilang <nathcanilang@gmail.com>
2024-03-03 13:32:28 +0800

    changed the connection string

--- TriforceSalon/Class Components/TransactionMethods.cs
+++ TriforceSalon/Class Components/TransactionMethods.cs
@@ -17,7 +17,7 @@ namespace TriforceSalon.Class_Components
         private int VariationID;
         public TransactionMethods()
         {
-            mysqlcon = "server=localhost;user=root;database=salondatabase;password=";
+            mysqlcon = "server=153.92.15.3;user=u139003143_salondatabase;database=u139003143_salondatabase;password=M0g~:^GqpI";
         }
 
         public void ProcessCustomer(string serviceName, int serviceID)
--- TriforceSalon/UserControls/EmployeeUserConrols.cs
+++ TriforceSalon/UserControls/EmployeeUserConrols.cs
@@ -30,7 +30,7 @@ namespace TriforceSalon.UserControls
 
             try
             {
-                using (var conn = new MySqlConnection("server=localhost;user=root;database=salondatabase;password="))
+                using (var conn = new MySqlConnection("server=153.92.15.3;user=u139003143_salondatabase;database=u139003143_salondatabase;password=M0g~:^GqpI"))
                 {
                     conn.Open();
 
