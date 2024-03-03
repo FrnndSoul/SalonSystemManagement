0b0c397a524c3f78577252b02a86f86fdede2b8b
FrnndSoul <125233489+FrnndSoul@users.noreply.github.com>
2024-03-03 22:04:53 +0800

    adminform debugged

--- TriforceSalon/MainForm.cs
+++ TriforceSalon/MainForm.cs
@@ -32,47 +32,61 @@ namespace TriforceSalon
 
         public void ShowLogin()
         {
+            walkInTransactionForm1.Visible = false;
             adminForm1.Visible = false;
             managerPage1.Visible = false;
             signinPage1.Visible = true;
             signUpForm1.Visible = false;
-            walkInTransactionForm1.Visible = false;
+            employeeUserConrols1.Visible = false;
         }
 
         public void ShowAdmin()
         {
+            walkInTransactionForm1.Visible = false;
             adminForm1.Visible = true;
             managerPage1.Visible = false;
             signinPage1.Visible = false;
             signUpForm1.Visible = false;
-            walkInTransactionForm1.Visible = false;
+            employeeUserConrols1.Visible = false;
         }
 
         public void ShowSignUp()
         {
+            walkInTransactionForm1.Visible = false;
             adminForm1.Visible = false;
             managerPage1.Visible = false;
             signinPage1.Visible = false;
             signUpForm1.Visible = true;
-            walkInTransactionForm1.Visible = false;
+            employeeUserConrols1.Visible = false;
         }
 
         public void ShowManager()
         {
+            walkInTransactionForm1.Visible = false;
             adminForm1.Visible = false;
             managerPage1.Visible = true;
             signinPage1.Visible = false;
             signUpForm1.Visible = false;
-            walkInTransactionForm1.Visible = false;
+            employeeUserConrols1.Visible = false;
         }
 
         public void ShowWalkIn()
         {
+            walkInTransactionForm1.Visible = true;
             adminForm1.Visible = false;
             managerPage1.Visible = false;
             signinPage1.Visible = false;
             signUpForm1.Visible = false;
-            walkInTransactionForm1.Visible = true;
+            employeeUserConrols1.Visible = false;
+        }
+        public void ShowEmployee()
+        {
+            walkInTransactionForm1.Visible = false;
+            adminForm1.Visible = false;
+            managerPage1.Visible = false;
+            signinPage1.Visible = false;
+            signUpForm1.Visible = false;
+            employeeUserConrols1.Visible = true;
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
